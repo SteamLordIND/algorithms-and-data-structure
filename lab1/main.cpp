733d08c5a93955e6dfe380a339b2b4462afbdd1b
Dmitry Bulatov <47477702+debugggger@users.noreply.github.com>
2021-02-12 15:37:54 +0300

    delete comments

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -4,27 +4,6 @@
 
 int main()
 {
-  /*DoubleLinkedList list;   // Создание пустого списка
-
-  list.insertHead(2);      // Добавление элементов
-  list.insertHead(3);
-  list.insertHead(1);
-  list.insertTail(4);
-  list.outAll();           // Печать элементов
-
-  std::cout << ((list.searchItem(1)) ? "1 find" : "1 not find") << std::endl;
-  std::cout << ((list.searchItem(8)) ? "8 find" : "8 not find") << std::endl;
-
-  DoubleLinkedList  list1; // (list);  // Копирование списка
-  list1.insertHead(4);         // Добавление элемента
-  list1.insertHead(5);         // Добавление элемента
-  list1.deleteHead();          // Удаление головного
-  list1.outAll();
-  list1.insertHead(6);
-  list1.deleteHead();
-  list1.outAll();        // Печать результата
-*/
-
   {//single linked list
     List list1;
     list1 += 10;
